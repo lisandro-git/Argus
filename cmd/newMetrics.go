bdb745cc58f02c70a2fcb045625ccab5b82bc9b9
Lisandro 'Edode' Chelidze <git-lisandro@x394.org>
2022-09-14 15:51:19 -0400

    Commented code

--- cmd/newMetrics.go
+++ cmd/newMetrics.go
@@ -9,6 +9,7 @@ var (
 	Gatherer prometheus.Gatherer  = NewGatherer(Registry)
 )
 
+// NewGaugeVec creates a new prometheus.GaugeVec
 func NewGaugeVec(name, help string, labels []string) *prometheus.GaugeVec {
 	return prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: name,
@@ -16,6 +17,7 @@ func NewGaugeVec(name, help string, labels []string) *prometheus.GaugeVec {
 	}, labels)
 }
 
+// NewGauge creates a new prometheus.Gauge
 func NewGauge(name, help string) prometheus.Gauge {
 	return prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: name,
@@ -23,14 +25,17 @@ func NewGauge(name, help string) prometheus.Gauge {
 	})
 }
 
+// NewRegisterer creates a new prometheus.Registerer
 func NewRegisterer() *prometheus.Registry {
 	return prometheus.NewPedanticRegistry()
 }
 
+// NewGatherer creates a new prometheus.Gatherer
 func NewGatherer(reg *prometheus.Registry) prometheus.Gatherer {
 	return prometheus.Gatherers{reg}
 }
 
+// RegisterCollector registers a prometheus.Collector
 func RegisterCollector(C interface{ prometheus.Collector }) {
 	Registry.MustRegister(C)
 }
